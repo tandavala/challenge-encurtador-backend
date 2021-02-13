217094d1b8f89adb9e15534399f4b5f63ef52741
tandavala <jose.tandavala@gmail.com>
2021-02-13 15:03:52 +0100

    adding dockerfiles

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
+// testando agora
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 5000);
 }
 bootstrap();
