81e766a178ef1f7f53454cd87d1b0c57f03ba5e0
tandavala <jose.tandavala@gmail.com>
2021-02-14 12:00:50 +0100

    passing test on UrlService create.

--- src/urls/urls.service.ts
+++ src/urls/urls.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { CreateUrlDto } from './dto/url.dto';
 import { Url } from './entiry/url.entity';
 
 @Injectable()
@@ -13,5 +14,10 @@ export class UrlsService {
   }
   async findOne(id: number): Promise<Url> {
     return this.urlRepo.findOneOrFail(id);
+  }
+  async create(createUrlDto: CreateUrlDto): Promise<Url> {
+    const newUrl = this.urlRepo.create(createUrlDto);
+    await this.urlRepo.save(newUrl);
+    return newUrl;
   }
 }
