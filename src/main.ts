978e050f311dfe8d6b3533f00c0920b5a53b69b8
tandavala <jose.tandavala@gmail.com>
2021-02-14 13:26:59 +0100

    setup documentation

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,28 @@
 
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import * as rateLimit from 'express-rate-limit';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Encurtador challenge Backend')
+    .setDescription(
+      `
+   Challenge encurtador Backend é um desafio da wiser educação 
+   a ideia é desenvolver uma API que irá receber como parâmetro 
+   uma URL que deverá ser encurtada. A URL encurtada deve aceitar 
+   o mínimo de 5 e o máximo de 10 caracteres e a URL deve apenas 
+   conter letras minúsculas`,
+    )
+    .setVersion('1.0')
+    .addTag('encurtador')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   app.use(
     rateLimit({
