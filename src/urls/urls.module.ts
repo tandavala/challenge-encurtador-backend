3dfdbf6b2428048b13adbff57acce6a7776e6e4e
tandavala <jose.tandavala@gmail.com>
2021-02-14 12:23:02 +0100

    generating urls controller

--- src/urls/urls.module.ts
+++ src/urls/urls.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Url } from './entiry/url.entity';
 import { UrlsService } from './urls.service';
+import { UrlsController } from './urls.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Url])],
   providers: [UrlsService],
+  controllers: [UrlsController],
 })
 export class UrlsModule {}
