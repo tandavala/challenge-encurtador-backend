d13c51d67d86a8c2a9988b0f0a6ddab2a99591d0
tandavala <jose.tandavala@gmail.com>
2021-02-14 12:37:28 +0100

    failing test o UrlController create.

--- src/urls/urls.controller.spec.ts
+++ src/urls/urls.controller.spec.ts
@@ -1,18 +1,64 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { CreateUrlDto } from './dto/url.dto';
 import { UrlsController } from './urls.controller';
+import { UrlsService } from './urls.service';
+
+const longUrl = 'https://google.com';
+const shortenUrl = 'hottopic';
 
 describe('UrlsController', () => {
   let controller: UrlsController;
+  let service: UrlsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UrlsController],
+      providers: [
+        {
+          provide: UrlsService,
+          useValue: {
+            findAll: jest.fn().mockResolvedValue([
+              { longUrl, shortenUrl },
+              { longUrl: 'https://twitter.com', shortenUrl: 'awesome' },
+            ]),
+            findOne: jest.fn().mockImplementation((id: number) =>
+              Promise.resolve({
+                longUrl: longUrl,
+                breed: shortenUrl,
+              }),
+            ),
+            getByShortenUrl: jest
+              .fn()
+              .mockImplementation((shortenUrl: string) =>
+                Promise.resolve({ longUrl, shortenUrl }),
+              ),
+            create: jest
+              .fn()
+              .mockImplementation((creaUrlDto: CreateUrlDto) =>
+                Promise.resolve({ longUrl, shortenUrl }),
+              ),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<UrlsController>(UrlsController);
+    service = module.get<UrlsService>(UrlsService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+  describe('create()', () => {
+    it('should create a new url', async () => {
+      const newUrl: CreateUrlDto = {
+        longUrl,
+        shortenUrl,
+      };
+      await expect(controller.create(newUrl)).resolves.toEqual({
+        id: 1,
+        ...newUrl,
+      });
+    });
   });
 });
